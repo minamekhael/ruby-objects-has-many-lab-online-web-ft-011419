35f9aff9c2a19057f8e33cc9d3772467eafe6396
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-31 03:08:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,8 +15,8 @@ class Artist
   end
   
   def add_song_by_name(song)
-    song = Song.new
-    self.songs << song
+    song = Song.new(song)
+    @songs << song
     song.artist = self
     @@song_count +=1
   end
