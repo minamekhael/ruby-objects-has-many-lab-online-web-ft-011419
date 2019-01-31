e1fd27e9e4aa1edc08a475328e3e0e405afcc78c
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-31 02:56:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,14 @@
 class Artist
   attr_accessor :name
   
+  @@songs = []
+  
   def initialize(name)
     @name = name
-    @songs = []
   end
   
   def add_song(song)
-   @songs << song
+   @@songs << song
   end
   
   def songs
